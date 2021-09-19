dd8ed5cb12b49bec71e9349c9a63629abd91fdbe
blakehilliard <blakesto@gmail.com>
2021-09-18 21:28:54 -0500

    Enter name in menu, don't allow start without it

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using static Exiter;
 
 public class MenuUI : MonoBehaviour
 {
+    private Text playerNameText;
+    private Button startButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton = GameObject.Find("Start Button").GetComponent<Button>();
+        playerNameText = GameObject.Find("Name Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        startButton.interactable = playerNameText.text.Length > 0;
     }
 
     public void StartGame()
